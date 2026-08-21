216eb951d3a403e8d5f7122210d5c388398a1f4b
unknown <Haznain666>
2026-08-22 03:19:33 +0500

    latest

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -98,7 +98,7 @@ export default function Footer() {
       <div className="border-t border-cream/10">
         <div className="container-x py-5 text-center">
           <p className="text-xs text-cream/50">
-            © 2026 IGA Sial Farm · Sponsor-a-Cow Program. All rights reserved. <a href="https://techsurgeinc.com/">Techsurge</a>
+            © 2026 IGA Sial Farm · Sponsor-a-Cow Program. All rights reserved. Developed by <a href="https://techsurgeinc.com/"><u>TechSurge Inc</u></a>
           </p>
         </div>
       </div>
